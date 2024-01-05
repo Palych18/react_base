a08c51ee8f3e1e10ee6a2e4495207bbdcd8c221e
Palych18 <pavel_egoroff@mail.ru>
2024-01-05 22:40:37 +0300

    Refactored getClassNames function

--- src/utils/getClassNames/getClassNames.js
+++ src/utils/getClassNames/getClassNames.js
@@ -1,50 +1,23 @@
 /**
- * @typedef {import('./types').ConditionalClassNames} ModClassNames
+ * @typedef {import('./types').ConditionalClassNames} CondClassNames
  */
 
 /**
  * @function getClassNames
  * @param {string} baseClassName
- * @param {ModClassNames} modClassNames
+ * @param {CondClassNames} condClassNames
  * @param {string[] | []} otherClassNames
  * @return {string}
  */
 
 export const getClassNames = (
   baseClassName,
-  modClassNames = {},
+  condClassNames = {},
   otherClassNames = [],
 ) => [
   baseClassName,
-  ...Object.entries(modClassNames)
-    .filter((mod) => Boolean(mod[1]))
-    .map((mod) => mod[0]),
+  ...Object.entries(condClassNames)
+    .filter((tuple) => Boolean(tuple[1]))
+    .map((tuple) => tuple[0]),
   ...otherClassNames.filter(Boolean),
 ].join(' ');
-
-// export const getClassNames = (
-//   baseClassName,
-//   modClassNames = {},
-//   otherClassNames = [],
-// ) => {
-//   let /** @type {string[] | []} */ arrClassNames = [];
-
-//   if (typeof baseClassName === 'string') {
-//     arrClassNames = [baseClassName];
-//   }
-//   if (typeof modClassNames === 'object' && modClassNames !== null) {
-//     for (const key in modClassNames) {
-//       if (modClassNames[ key ]) {
-//         arrClassNames = [...arrClassNames, key];
-//       }
-//     }
-//   }
-//   if (Array.isArray(otherClassNames)) {
-//     arrClassNames = [...arrClassNames, ...otherClassNames];
-//   }
-//   const classNames = arrClassNames.length > 1
-//     ? arrClassNames.join(' ')
-//     : arrClassNames[0];
-
-//   return classNames;
-// };
